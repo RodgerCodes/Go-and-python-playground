c5c1ad511da4ca225f8917a343d62935d865135b
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2024-01-09 19:19:45 +0200

    chore: make a post request with JSON data

--- api/main.go
+++ api/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 )
 
 func main() {
+	post()
 	url := "https://jsonplaceholder.typicode.com/posts"
 
 	// basically takes request type, api endpoint and request body
@@ -25,7 +27,36 @@ func main() {
 	}
 
 	defer res.Body.Close()
+	_, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		fmt.Println(readErr.Error())
+	}
+
+	// fmt.Println(string(body))
+
+}
+
+func post() {
+	url := "https://jsonplaceholder.typicode.com/posts"
+	testData := []byte(`{"title":"test title", "body":"this is the body"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(testData))
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	req.Header.Add("Content-type", "application/json; charset=UTF-8")
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	defer res.Body.Close()
+
 	body, readErr := io.ReadAll(res.Body)
+
 	if readErr != nil {
 		fmt.Println(readErr.Error())
 	}
