70e8bf60beb6f6753dc50aaac7247242a2499045
Rodger kumwanje <rkumwanje@gmail.com>
2024-05-12 22:00:16 +0200

    feat: downloads dir file sorter

--- sorter/main.go
+++ sorter/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -13,16 +14,40 @@ func main() {
 		fmt.Println(err)
 	}
 
-	newHomeDir := homeDir + "/Downloads"
+	downloadDir := homeDir + "/Downloads"
 
-	files, err := os.ReadDir(newHomeDir)
+	files, err := os.ReadDir(downloadDir)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	for _, file := range files {
-		fmt.Println(file.Name())
+
+		fileExt := filepath.Ext(file.Name())
+		fileDir := downloadDir + "/" + file.Name()
+
+		if fileExt == ".mkv" || fileExt == ".mp4" {
+			videoDir := homeDir + "/Videos/" + file.Name()
+			moveFileToNewDir(fileDir, videoDir)
+		} else if fileExt == ".png" || fileExt == ".jpeg" || fileExt == ".svg" || fileExt == ".jpg" {
+			imageDir := homeDir + "/Pictures/" + file.Name()
+			moveFileToNewDir(fileDir, imageDir)
+		} else {
+			documentsDir := homeDir + "/Documents/" + file.Name()
+			moveFileToNewDir(fileDir, documentsDir)
+		}
 	}
 
+	fmt.Println("Successfully organized downloads directory")
+
+}
+
+func moveFileToNewDir(oldPath string, newPath string) error {
+	e := os.Rename(oldPath, newPath)
+	if e != nil {
+		return e
+	}
+
+	return nil
 }
